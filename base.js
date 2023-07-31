c9d19f07e171d18ef183c50ea022cea6ada125b8
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-07-31 10:49:03 -0300

    .

--- base.js
+++ base.js
@@ -54,7 +54,6 @@ function completarAño(texto) {
     return texto.replace(/([89]\d)$/g, "19$1").replace(/([012]\d)$/g, "20$1");
 }
 
-
 function comienzaConMayuscula(palabra) {
     const patron = /^[A-Z]/;
     return patron.test(palabra);
@@ -87,6 +86,102 @@ function esSoloOrdenanzaValida(texto) {
     const ordenanza = normalizarOrdenanza(texto);
 
     return ordenanzas.some(o => o.ordenanza == ordenanza);
+}
+
+// Mediciones
+
+let mediciones = [];
+
+function medir(titulo = "Ejecutando") {
+    mostrar(`> ${titulo}`);
+    mediciones.push(new Date());
+}
+
+function mostrar(...parametros) {
+    const nivel = '   '.repeat(mediciones.length);
+    console.log(nivel, ...parametros);
+}
+
+function fin() {
+    const inicio = mediciones.pop();
+    mostrar(`| ${new Date() - inicio}ms`);
+}
+
+// WEB
+
+const plantillas = {};
+
+async function cargarJson(origen) {
+    const response = await fetch(`./datos/${origen}.json`);
+    return await response.json();
+}
+
+async function cargarOrdenanza(ordenanza) {
+    const origen = generarUrl(ordenanza, 'html', true);
+    const parser = new DOMParser();
+    const response = await fetch(origen);
+    const html = parser.parseFromString((await response.text()), 'text/html');
+    let texto = html.body.innerHTML;
+
+    return texto;
+}
+
+function cargarPlantilla(Plantilla) {
+    plantillas[Plantilla] ||= Handlebars.compile(document.getElementById(Plantilla).innerHTML);
+    return plantillas[Plantilla];
+}
+
+// permite que el usuario baje un archivo
+
+function enviarWhatsapp(ordenanza) {
+    let texto = `
+*Digesto Digital Yerba Buena*
+    
+Bajar la ordenanza ${ordenanza} de 
+${generarUrl(ordenanza, 'pdf')}`;
+
+    texto = encodeURIComponent(texto);
+    url = `https://api.whatsapp.com/send/?text=${texto}&type=custom_url&app_absent=0`;
+
+    invocarUrl(url, '', true);
+}
+
+function descargarJson(json, destino) {
+    const contenidoJSON = JSON.stringify(json);
+
+    const blob = new Blob([contenidoJSON], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    invocarUrl(url, destino, false);
+
+    URL.revokeObjectURL(url);
+}
+
+function descargarPdf(ordenanza) {
+    ordenanza = normalizarOrdenanza(ordenanza)
+    const url = generarUrl(ordenanza, 'pdf');
+    invocarUrl(url, `${ordenanza}.pdf`, true);
+}
+
+function generarUrl(ordenanza, tipo, local = false) {
+    const base = local ? "." : 'https://digestoyb.netlify.app';
+
+    // ordenanza = normalizarOrdenanza(ordenanza);
+    tipo = tipo.toLowerCase();
+
+    return `${base}/${tipo}/${ordenanza}.${tipo}`;
+}
+
+function invocarUrl(url, destino = null, abrir = false) {
+    const enlace = document.createElement('a');
+
+    enlace.href = url;
+    if (destino) enlace.download = destino;
+    if (abrir) enlace.target = "_blank"
+
+    document.body.appendChild(enlace);
+    enlace.click();
+    document.body.removeChild(enlace);
 }
 
 function esSoloClasificacion(texto) {
